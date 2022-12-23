76e34de2b0fdfd9f1a57fce1b04ff2e3ef88179a
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-12-23 19:02:57 +0100

    A new commit

--- Extension.js
+++ Extension.js
@@ -15,10 +15,12 @@ if (leadsFromLocalStorage){
 function render(leads){
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
-        listItems += `
-        <li>
-            <a target='_blank' href='  ${leads[i]} '> ${leads[i]} </a>
-        </li>`
+        listItems += 
+        `
+            <li>
+                <a target='_blank' href='  ${leads[i]} '> ${leads[i]} </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems
 }
