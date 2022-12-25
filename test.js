9f14475d513be12bda541e6ef3d492874f98d439
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2022-12-25 04:03:23 +0100

    A new commit

--- test.js
+++ test.js
@@ -31,6 +31,34 @@
 // }
 // guy(myCourses)
 
-localStorage.setItem("good","How are you")
-let get = localStorage.getItem("good")
-console.log(get)
+// localStorage.setItem("good","How are you")
+// let get = localStorage.getItem("good")
+// console.log(get)
+
+// async function getstuff(){
+//     const { Configuration, OpenAIApi } = require("openai");
+//     const configuration = new Configuration({
+//         apiKey: process.env.OPENAI_API_KEY,
+//     });
+//     const openai = new OpenAIApi(configuration);
+//     const response = await openai.listFiles();
+//     return response
+// }
+// console.log(getstuff())
+
+// let first = 0;
+// let second = 0;
+
+// function increase(){
+//     second += 1
+//     if (second === 10){
+//         first += 1
+//         second -= second
+//         second = 0
+//     }
+//     return `${first.toString().padStart(2, '0')} : ${second.toString().padStart(2, '0') }`
+// }
+
+// setInterval(() => {
+//     //console.log(increase())
+// }, 1000);
